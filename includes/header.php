6b1b5b8ccaa74abcdbe0740fd16edaa8b4a196f5
AlexJog <alexdu6610@gmail.com>
2026-02-20 11:30:11 +0100

    ajout header  balise meta

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vite & Gourmand - Traiteur à Bordeaux</title>
+    <meta name="description" content="Vite & Gourmand, traiteur à Bordeaux spécialisé en brunchs, mariages et événements. Commandez en ligne des plats frais et faits maison.">
     
     <!-- Google Fonts pour police d'écriture -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
